80cb644fbe236dfe0faea6339bd62b766f6b1063
kamruzzamanripon <rkamruzzaman@gmail.com>
2024-09-18 19:48:39 +0600

    update testarea features

--- resources/js/Components/TextAreaInput.jsx
+++ resources/js/Components/TextAreaInput.jsx
@@ -10,13 +10,13 @@ export default forwardRef(function TextAreaInput({ className = '', isFocused = f
     }, []);
 
     return (
-        <input
+        <textarea
             {...props}
             className={
                 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
                 className
             }
             ref={input}
-        />
+        ></textarea>
     );
 });
